7380b271006aa66de68e9ebf88a2351123e6659c
Jagadeesh R <58656215+JagadeeshRelli@users.noreply.github.com>
2024-08-31 17:38:28 +0530

    Update etl_dag.py

--- docker/dags/etl_dag.py
+++ docker/dags/etl_dag.py
@@ -97,8 +97,8 @@ def load_data():
     db_password = os.getenv('DB_PASSWORD')
     db_host = os.getenv('DB_HOST')
     db_name = os.getenv('DB_NAME')
-
-    engine = create_engine(f'postgresql+psycopg2://{db_user}:{db_password}@{db_host}:5432/{db_name}')
+    #replace port_name with your port name
+    engine = create_engine(f'postgresql+psycopg2://{db_user}:{db_password}@{db_host}:<port_name>/{db_name}')
     
     upsert_users_query = """
     INSERT INTO users (userid, name, contact, balance)
